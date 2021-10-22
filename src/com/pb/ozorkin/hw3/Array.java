e7172b89bbd1f2530e0bdd7bfca9024b54d89114
Ruslan Ozorkin <ruslan.ozerkin@privatbank.ua>
2021-10-22 14:10:00 +0300

    решение задания 3 пузырьки

---
+++ src/com/pb/ozorkin/hw3/Array.java
@@ -0,0 +1,36 @@
+package com.pb.ozorkin.hw3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[10];
+        System.out.println("Введите число для массива:");
+        int x = 0;
+        for (int i = 0; i < 10; i++) {
+            array[i] = scanner.nextInt();
+        }
+        for (int y : array) {
+            x = x + y;
+        }
+        System.out.println("сумма всех элементов массива: " + x);
+
+        boolean isSorted = false;
+        int z;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    isSorted = false;
+
+                    z = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = z;
+                    System.out.println(Arrays.toString(array));
+                }
+            }
+        }
+    }
+}
