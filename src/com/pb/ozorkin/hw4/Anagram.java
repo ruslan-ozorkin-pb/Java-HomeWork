52e900e66e0a810f4ab851a3fb8c089145626ec3
Ruslan Ozorkin <ruslan.ozerkin@privatbank.ua>
2021-10-28 11:50:06 +0300

    задание 4.2 выполнено

--- src/com/pb/ozorkin/hw4/Anagram.java
+++ src/com/pb/ozorkin/hw4/Anagram.java
@@ -2,34 +2,30 @@ package com.pb.ozorkin.hw4;
 
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class Anagram {
+    static char[] meth(String A) {
+                return A.toCharArray(); // методом раскладываем строку на массив
+            }
+            static String meth2 (String B) { return B.replaceAll("[^\\p{L}\\p{N}]+", "");} // удаляем пробелы и симв
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String str1 = scanner.nextLine(); // ввод 1 строки
-        String str2 = scanner.nextLine(); // ввод 2 строки
-        char[] array = str1.toCharArray(); // раскладываем строку на массив символов
-        for (int i = 0; i < array.length; i++) { // задаем условие вывода массива 1
-            System.out.println(array[i]);
-        }
-        char[] array2 = str2.toCharArray(); // раскладываем строку на массив символов
-        for (int i = 0; i < array.length; i++) { // задаем условие вывода массива 2
-            System.out.println(array2[i]);
-/// Первая часть правильная, над второю подумаем)
-        }
-//        System.out.println(str1.equalsIgnoreCase(str2)); // вот тут сравнение не идет :(
+        String str1 = scanner.nextLine().toLowerCase(); // ввод 1 строки
+        String str2 = scanner.nextLine().toLowerCase(); // ввод 2 строки
+        char[] array = meth(str1); // раскладываем строку на массив символов
+        char[] array2 = meth(str2); // раскладываем строку на массив символов
+            Arrays.sort(array); // сортируем буквы
+            Arrays.sort(array2);
+            String sort = new String (array);
+            String sort2 = new String (array2);
+        System.out.println(meth2(sort));
+        System.out.println(meth2(sort2));
+                if (meth2(sort).equals(meth2(sort2))) {
+            System.out.println("anagram");} else {
+        System.out.println("not anagram"); }
+
+
+
     }
-        public boolean isEqual ( char[] array, char[] array2){
-            if (Arrays.equals(array, array2)) {
-                return true;
-            }
-            return false;
-        }
-
-
-
-
-
-
 
 }
