8cd2ad2dd0f408a27ab4c2f53a372a54ca8f8fb6
Ruslan Ozorkin <ruslan.ozerkin@privatbank.ua>
2022-01-12 11:15:06 +0200

    задание 15

---
+++ src/com/pb/ozorkin/hw15_1/Server.java
@@ -0,0 +1,68 @@
+package com.pb.ozorkin.hw15_1;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Server {
+    static final int PORT = 1234;
+    private ArrayList<ClientHandler> clients = new ArrayList<ClientHandler>();
+    public Server() {
+
+        Socket clientSocket = null;
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Сервер запущен!");
+            while (true) {
+                clientSocket = serverSocket.accept();
+                ClientHandler client = new ClientHandler(clientSocket, this);
+                clients.add(client);
+                new Thread(client).start();
+            }
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            try {
+                clientSocket.close();
+                System.out.println("Сервер остановлен");
+                serverSocket.close();
+            }
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public void sendMessageToAllClients(String msg) {
+        for (ClientHandler o : clients) {
+            o.sendMsg(msg);
+        }
+
+    }
+
+    public void removeClient(ClientHandler client) {
+
+        clients.remove(client);
+    }
+    public static void main(String[] args) {
+
+        Server server = new Server();
+    }
+
+}
+
